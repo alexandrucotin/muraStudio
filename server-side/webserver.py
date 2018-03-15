abc723bcfb5fb19162c36091d580f5249f64353e
Riccardo <Riccardo@DESKTOP-CE2H1UN.station>
2018-03-15 11:18:24 +0100

    added placeholder files

--- server-side/webserver.py
+++ server-side/webserver.py
@@ -1,1 +1,13 @@
+from flask import Flask, send_from_directory
+from flask_sslify import SSLify
 
+app = Flask(__name__)
+ssLify = SSLify(app)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return send_from_directory('../client-side/html/', 'home.html')
+
+if __name__ == '__main__':
+    app.run(threaded = True)
