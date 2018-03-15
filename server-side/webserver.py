e7f3be9f5d013f5ac2939946db3913b7065b0190
Riccardo <Riccardo@DESKTOP-CE2H1UN.station>
2018-03-15 11:39:01 +0100

    serving bootstrap

--- server-side/webserver.py
+++ server-side/webserver.py
@@ -9,5 +9,21 @@ ssLify = SSLify(app)
 def home():
     return send_from_directory('../client-side/html/', 'home.html')
 
+@app.route('/bootstrap/<nome_file>')
+def bootstrap(nome_file):
+    return send_from_directory('../client-side/bootstrap/', nome_file)
+
+@app.route('/bootstrap/css/<nome_file>')
+def bootstrap_css(nome_file):
+    return send_from_directory('../client-side/bootstrap/css/', nome_file)
+
+@app.route('/bootstrap/vendor/bootstrap/<cartella>/<nome_file>')
+def bootstrap_vendor(cartella, nome_file):
+    return send_from_directory('../client-side/bootstrap/vendor/bootstrap/' + cartella + '/', nome_file)
+
+@app.route('/bootstrap/vendor/jquery/<nome_file>')
+def jquery(nome_file):
+    return send_from_directory('../client-side/bootstrap/vendor/jquery/', nome_file)
+
 if __name__ == '__main__':
     app.run(threaded = True)
