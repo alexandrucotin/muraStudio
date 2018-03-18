208121dfb719961b30cf1d7bbb273fa7e00b4451
alexandrucotin <a.cotin96@yahoo.com>
2018-03-18 15:20:02 +0100

    add post and work pages

--- client-side/js/scrollSpy.js
+++ client-side/js/scrollSpy.js
@@ -1,8 +1,8 @@
 /**
- * Listen to scroll to change header opacity class
- */
+ * Listen to scroll to change header opacity class*/
+
 function checkScroll(){
-    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+    var startY = $('.navbar').height() + 5; //The point where the navbar changes in px
 
     if($(window).scrollTop() > startY){
         $('.navbar').addClass("scrolled");
@@ -11,8 +11,8 @@ function checkScroll(){
     }
 }
 
-if($('.navbar').length > 0){
+if ( $('.navbar').length > 0){
     $(window).on("scroll load resize", function(){
         checkScroll();
     });
-}
+}  
