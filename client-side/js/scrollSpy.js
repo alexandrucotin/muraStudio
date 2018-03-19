04cb84bcf4adaefb4bc191fb2df6e45e95dd0060
alexandrucotin <a.cotin96@yahoo.com>
2018-03-19 11:37:54 +0100

    modify scrollspy color 2

--- client-side/js/scrollSpy.js
+++ client-side/js/scrollSpy.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $(".navbar-default").css('background-color', 'rgba(0, 0, 0, 0.58)');
+          $(".navbar-default").css('background-color', 'rgb(0, 0, 0, 0.58)');
        } else {
           $('.navbar-default').css('background-color', 'transparent');
        }
