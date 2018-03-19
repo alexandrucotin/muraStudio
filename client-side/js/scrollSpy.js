f958c52df27f6cbf2f3b5a25f39304ae566e9c7d
Riccardo <Riccardo@192.168.1.31>
2018-03-19 10:34:53 +0100

    modified scrollspy

--- client-side/js/scrollSpy.js
+++ client-side/js/scrollSpy.js
@@ -2,14 +2,14 @@
  * Listen to scroll to change header opacity class*/
 
 function checkScroll(){
-    var startY = $('.navbar').height() + 5; //The point where the navbar changes in px
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
     if($(window).scrollTop() > startY){
         $('.navbar').addClass("scrolled");
     }else{
         $('.navbar').removeClass("scrolled");
     }
-}
+};
 
 if ( $('.navbar').length > 0){
     $(window).on("scroll load resize", function(){
