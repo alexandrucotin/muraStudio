8046cb5e5495465b21ea1a534e85bbbf747a54be
alex cotin <a.cotin96@yahoo.com>
2018-03-25 00:34:12 +0100

    changed background navbar

--- client-side/js/scrollSpy.js
+++ client-side/js/scrollSpy.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $(".navbar-default").css('background-color', 'rgba(146, 89, 166, 0.6)');
+          $(".navbar-default").css('background-color', 'rgba(255, 255, 255, 0.7)');
        } else {
           $('.navbar-default').css('background-color', 'transparent');
        }
