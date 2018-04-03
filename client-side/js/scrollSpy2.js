825e3fdd59931eaa556b08db572d0c3d82c1bcf9
alex cotin <a.cotin96@yahoo.com>
2018-04-03 14:18:30 +0200

    added scrollspy2

---
+++ client-side/js/scrollSpy2.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if($(window).width() < 960) {
+          $(".navbar-default").css('background-color', 'rgba(255, 255, 255, 0.93)');
+          $(".navbar-brand-custom").css('color', 'black');
+          $(".item").css('color', 'black!important');
+       } else if ((scroll_start > offset.top ) && ($(window).width() > 960) ) {
+           $(".navbar-default").css('background-color', 'rgba(255, 255, 255, 0.93)');
+          $(".navbar-brand-custom").css('color', 'black');
+           $(".item").css('color', 'black!important');
+         
+       } else {
+            $('.navbar-default').css('background-color', 'transparent');
+       }
+   });
+    }
+});
