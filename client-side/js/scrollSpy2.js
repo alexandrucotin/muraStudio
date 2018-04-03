d45e118d63b6f0b72c6a5e5427df9d79032b0e9c
alex cotin <a.cotin96@yahoo.com>
2018-04-03 15:35:58 +0200

    modify scroll spy 2

--- client-side/js/scrollSpy2.js
+++ client-side/js/scrollSpy2.js
@@ -8,14 +8,14 @@ $(document).ready(function(){
       if($(window).width() < 960) {
           $(".navbar-default").css('background-color', 'rgba(255, 255, 255, 0.93)');
           $(".navbar-brand-custom").css('color', 'black');
-          $(".item").css('color', 'black!important');
+          $(".item").css('color', 'black');
        } else if ((scroll_start > offset.top ) && ($(window).width() > 960) ) {
-           $(".navbar-default").css('background-color', 'rgba(255, 255, 255, 0.93)');
+          $(".navbar-default").css('background-color', 'rgba(255, 255, 255, 0.93)');
           $(".navbar-brand-custom").css('color', 'black');
-           $(".item").css('color', 'black!important');
+          $(".item").css('color', 'black');
          
        } else {
-            $('.navbar-default').css('background-color', 'transparent');
+          $('.navbar-default').css('background-color', 'transparent');
        }
    });
     }
