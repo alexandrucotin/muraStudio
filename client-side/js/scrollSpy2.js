9661cb03199c2f66baf85ad2f06dbc55849cdab0
alex cotin <a.cotin96@yahoo.com>
2018-04-07 15:54:13 +0200

    changed scrollspy

--- client-side/js/scrollSpy2.js
+++ client-side/js/scrollSpy2.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){       
-   var scroll_start = 0;
-   var startchange = $('#startchange');
-   var offset = startchange.offset();
-    if (startchange.length){
-   $(document).scroll(function() { 
-      scroll_start = $(this).scrollTop();
-      if($(window).width() < 960) {
-          $(".navbar").css('background-color', 'rgba(255, 255, 255, 0.93)');
-          $(".navbar-brand-custom").css('color', 'black');
-          $(".item").css('color', 'black!important');
-       } else if ((scroll_start > offset.top ) && ($(window).width() > 960) ) {
-          $(".navbar").css('background-color', 'rgba(255, 255, 255, 0.93)');
-          $(".navbar-brand-custom").css('color', 'black');
-          $(".item").css('color', 'black!important');
-         
-       } else {
-          $('.navbar').css('background-color', 'transparent');
-       }
-   });
+    var scroll_start = 0;
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
+    if ($(window).width() < 960) {
+        $(".navbar").css('background-color', 'rgba(255, 255, 255, 0.93)');
+        $(".navbar-brand-custom").css('color', 'black');
+        $(".item").css('color', 'black!important');
+    } else if (startchange.length){
+        $(document).scroll(function() { 
+           scroll_start = $(this).scrollTop();
+           if ((scroll_start > offset.top ) && ($(window).width() > 960) ) {
+              $(".navbar").css('background-color', 'rgba(255, 255, 255, 0.93)');
+              $(".navbar-brand-custom").css('color', 'black');
+              $(".item").css('color', 'black!important');
+
+           } else {
+              $('.navbar').css('background-color', 'transparent');
+           }
+       });
     }
 });
