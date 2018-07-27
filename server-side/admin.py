83c8d8eba529201de8878195bc494e81256d4467
adaschevici <a.daschevici@gmail.com>
2018-07-27 22:02:56 +0100

    Add initial Dockerfiles and basic nginx configuration

--- server-side/admin.py
+++ server-side/admin.py
@@ -23,7 +23,10 @@ class Admin:
             presence = self.manager.read_presence('SELECT * FROM user')
             if not presence:
                 salt = self.generate_salt()
-                password = sha256(sha256('admin').hexdigest() + salt + self.pepper).hexdigest()
+                password = sha256(
+                        (sha256('admin'.encode('utf-8')).hexdigest() +
+                        salt + self.pepper).encode('utf-8')
+                    ).hexdigest()
                 self.manager.write('''
                     INSERT INTO user (username, password, salt)
                     VALUES (?, ?, ?)
@@ -231,7 +234,7 @@ class Admin:
             DELETE FROM work_image
             WHERE work_id = ?
         ''', (element_id,))
-    
+
     # Change password
     def change_password(self, username, password):
         salt = self.generate_salt()
